8ca56e28b8cabe6812ca0b8d5c281dd56fbfebb5
Sameer-web-2006 <samasameer124@gmail.com>
2025-06-06 16:37:24 +0530

    Added day-night system

--- script.js
+++ script.js
@@ -5,13 +5,17 @@ const run = () => {
     let hour = now.getHours();
     let minute = now.getMinutes();
     let second = now.getSeconds();
+    let meri = "AM"
 
-    if (hour > 12) hour -= 12;
+    if (hour > 12){ 
+      hour -= 12;
+      meri = "PM";
+    };
     if (hour < 10) hour = "0" + hour;
     if (minute < 10) minute = "0" + minute;
     if (second < 10) second = "0" + second;
 
-    clock.innerText = `${hour} : ${minute} : ${second}`;
+    clock.innerText = `${hour} : ${minute} : ${second} ${meri}`;
 };
 
 setInterval(run, 900);
